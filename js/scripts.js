3c18421f996cda8fba959d70c53bce82b3c47c38
Diego Silva <diegossilva2008@gmail.com>
2026-05-27 14:05:05 -0300

    feat: JavaScript iniciado, com atributos DOM, QuerySelector e validação de entrada

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log("Teste");
+//elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+
+//funções
+
+//eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const multiplicationNumber = +numberInput.value
+    const multiplicatorNumber = +multiplicationInput.value
+
+    if (!multiplicationNumber || !multiplicatorNumber) {
+        return        
+    }
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+    
+
+})
